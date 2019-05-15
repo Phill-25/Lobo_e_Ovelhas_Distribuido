7cffbd38d8bae629e6a6eb5a7a0329c38f763136
Felipe-Dias-Mendes <48762331+Felipe-Dias-Mendes@users.noreply.github.com>
2019-05-15 19:02:29 -0300

    Update Servidor2.java

--- lobo_e_ovelhas/src/servidores/Servidor2.java
+++ lobo_e_ovelhas/src/servidores/Servidor2.java
@@ -8,6 +8,8 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
+import clientes.Cliente1;
+import clientes.Cliente2;
 import servidorInterface.ServidorInterface;
 
 @SuppressWarnings("serial")
@@ -77,6 +79,33 @@ public class Servidor2 extends UnicastRemoteObject implements ServidorInterface 
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+
+public int[][] tabuleiroInicial() {
+	
+	int[][] tabuleiro = {
+			{-2, 1, -2, 1, -2, 1, -2, 1}, // 8
+			{0, -2, 0, -2, 0, -2, 0, -2}, // 7
+			{-2, 0, -2, 0, -2, 0, -2, 0}, // 6
+			{0, -2, 0, -2, 0, -2, 0, -2}, // 5
+			{-2, 0, -2, 0, -2, 0, -2, 0}, // 4
+			{0, -2, 0, -2, 0, -2, 0, -2}, // 3
+			{-2, 0, -2, 0, -2, 0, -2, 0}, // 2
+			{0, -2, 0, -2, 8, -2, 0, -2} // 1
+//			 a, b,  c, d,  e,  f,  g, h
+	};	 
+	
+//	-2 são posições invalidas
+//	0 são posições validas
+//	1 são as ovelhinhas
+//	8 é o Lobão
+	
+	return tabuleiro;
+	
+	}
+	
+	public void Jogo(int[][] tabuleiro, Cliente1 C1, Cliente2 C2) {
 		
 	}
 
